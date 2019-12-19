d7c113bd0ac6ff0ba7013c2b89fa54dbc85b5dc8
Braham Moussouni <braham.moussouni@etudiant.univ-lr.fr>
2019-12-19 19:36:21 +0100

    NodeJS_API_REST_V1_Complete
    Implémentation des fonctionnalités ADD,UPDATE by id,DELETE by id,RESEARCH by id en utilisant les méthodes GET et POST

--- API REST/API.js
+++ API REST/API.js
@@ -1,5 +1,14 @@
 //L'application requiert l'utilisation du module Express.
-//La variable express nous permettra d'utiliser les fonctionnalités du module Express.  
+//La variable express nous permettra d'utiliser les fonctionnalités du module Express.
+var express = require('express'); 
+
+var bodyParser = require("body-parser"); 
+
+var fs = require('fs');
+
+var listep=[]; //liste des objets produits remplace provisoirement la BDD
+
+//La Classe Produit
 var Produit = function (id,nom,type,price,raiting,warranty_years,available) {
   var _id = id;
   var _nom = nom;
@@ -25,12 +34,12 @@ var Produit = function (id,nom,type,price,raiting,warranty_years,available) {
     _raiting = raiting;
   };
     
-  this.setNom = function (nom) {
-    _nom = nom;
+  this.setWarranty_years = function (warranty_years) {
+    _warranty_years = warranty_years;
   };
     
-  this.setNom = function (nom) {
-    _nom = nom;
+  this.setType = function (type) {
+    _type = type;
   };
     
   this.setAvailable = function (available) {
@@ -74,72 +83,131 @@ var Produit = function (id,nom,type,price,raiting,warranty_years,available) {
   };
 };
 
-var listep=[];
-var fs = require('fs');
+
+
+
+//Lecture du fichier JSON et création des Objet JSON 
 var obj = JSON.parse(fs.readFileSync('Products.json', 'utf8'));
+//Parcours des objets JSON et création d'objets Produits que je stock par la suite dans l'arraylist dédié à ces derniers 
 for(var i= 0; i < obj.length; i++)
-{
-    
-     
-    console.log(obj[i]);
+{   
     listep.push(new Produit(obj[i]._id,obj[i].name,obj[i].type,obj[i].price,obj[i].rating,obj[i].warranty_years,obj[i].available));
 }
-for(var i= 0; i < listep.length; i++)
-{ 
-    console.log(listep[i].toString());
-}
+
     
 
-var express = require('express'); 
-// Nous définissons ici les paramètres du serveur.
+
+// Je défini ici les paramètres du serveur.
 var hostname = 'localhost'; 
 var port = 3000; 
  
-// Nous créons un objet de type Express. 
+// Je crée un objet de type Express. 
 var app = express(); 
 
-var bodyParser = require("body-parser"); 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //Afin de faciliter le routage (les URL que nous souhaitons prendre en charge dans notre API), nous créons un objet Router.
 //C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes. 
 var myRouter = express.Router(); 
  
-// Je vous rappelle notre route (/produits).  
+// Je rappel la route (/produits).  
 myRouter.route('/produits')
-// J'implémente les méthodes GET, POST
-// GET
-.get(function(req,res){ 
- res.json({
- message : "Liste de tous les produits:"});}
-    )
+// J'implémente les méthodes GET & POST
+// GET affichage de tous les produits
+.get(function(req,res){      
+ res.json({message : "Liste de tous les produits:" + '\n' +listep.toString()});});
 
-//POST
-.post(function(req,res){
- res.json({message : "Ajoute d'un produit à la liste", 
- produit_id : req.body.produit_id, 
- produit_nom : req.body.produit_nom});
-});
- 
-// Je vous rappelle notre route (/). 
+
+// Je rappel la route (/). 
 myRouter.route('/')
-// all permet de prendre en charge toutes les méthodes. 
+// all permet de prendre en charge toutes les méthodes.
+// Acceuil
 .all(function(req,res){ 
-      res.json({message : "Bienvenue sur l'API REST ", methode : req.method});
+      res.json({message : "Bienvenue sur l'API_REST pour voir la liste des produits utilisez la route /produits, pour intéragir sur les objets utilisez la route /produits/action", methode : req.method});
 });
 
-// Je vous rappelle notre route (/produits/:produit_id'). 
-myRouter.route('/produits/:produit_id')
-// J'implémente les méthodes GET, PUT, DELETE.
-.get(function(req,res){ 
-	  res.json({message : "Vous souhaitez accéder aux informations du produit n°" + req.params.produit_id});
-})
-.put(function(req,res){ 
-	  res.json({message : "Vous souhaitez modifier les informations de la produit n°" + req.params.produit_id});
-})
-.delete(function(req,res){ 
-	  res.json({message : "Vous souhaitez supprimer la produit n°" + req.params.produit_id});
+// Je rappel la route (/produits/action'). 
+myRouter.route('/produits/action')
+.all(function(req,res){ 
+      res.json({message : "Liste des actions /add, /recherche?id=, /update, /delete ", methode : req.method});
 });
+
+
+myRouter.route('/produits/action/recherche')
+// J'implémente les méthodes GET, PUT, DELETE.
+// Get pour afficher un produit avec un id précis
+.get(function(req,res){ 
+    for(var i= 0; i < listep.length; i++)
+    {   
+        if( listep[i].getId() == req.query.id)
+        {
+            var affiche = listep[i].toString();
+            break;
+        }
+}
+	  res.json({message : "Vous souhaitez accéder aux informations du produit n°" , methode : req.method, affiche});
+
+    
+});
+
+myRouter.route('/produits/action/add')
+//POST ajouter un produit
+.post(function(req,res){
+ res.json({message : "Ajout d'un produit à la liste", 
+ id : req.body.id, 
+ nom : req.body.nom,
+ type : req.body.type,
+ price : req.body.price,
+ raiting : req.body.raiting,
+ warranty_years : req.body.warranty_years,
+ available : req.body.available});
+ 
+ listep.push(new Produit(req.body.id,req.body.nom,req.body.type,req.body.price,req.body.raiting,req.body.warranty_years,req.body.available));
+ console.log(listep[listep.length - 1].toString());
+});
+
+
+
+myRouter.route('/produits/action/update')
+//modifier un produit avec un id précis
+.post(function(req,res){ 
+    for(var i= 0; i < listep.length; i++)
+    {   
+        if( listep[i].getId() == req.body.id)
+        {
+            listep[i].setNom(req.body.nom);
+            listep[i].setPrice(req.body.price);
+            listep[i].setRaiting(req.body.raiting);
+            listep[i].setType(req.body.type);
+            listep[i].setWarranty_years(req.body.warranty_years);
+            listep[i].setAvailable(req.body.available);
+              res.json({message : "Vous avez modifier les informations du produit n°",
+     id : req.body.id});
+            break;
+        }
+        
+}
+});
+
+myRouter.route('/produits/action/delete')
+//Post pour supprimer un produit avec un id précis
+.post(function(req,res){ 
+    console.log("post");
+    for(var i= 0; i < listep.length; i++)
+    {   
+        if( listep[i].getId() == req.body.id)
+        {
+            delete listep[i];
+            console.log(listep.toString());
+            res.json({message : "Vous avez supprimer le produit n°" ,id : req.body.id ,  methode : req.method});
+            break;
+        }
+}
+	  
+    
+});
+
 
 
 
